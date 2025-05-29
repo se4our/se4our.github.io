9fdd636fa9e5a2197afff3b5b235b2da456c6414
se4our <klineftpley@gmail.com>
2025-05-29 22:03:52 +0800

    web-project v.0.9: Pre-Release
    Добавлена полноценно новая страница со случайными историческими фактами на английском. Создано с помощью ассинхронного программирования и запросом данных с данного API: https://api.api-ninjas.com. Также изменён вывод результата в карточке.

--- js/fact.js
+++ js/fact.js
@@ -7,16 +7,37 @@
         let month = parseInt(1 + Math.random() * 12);
         let day = parseInt(Math.random() * 30);
         const url = "https://api.api-ninjas.com/v1/historicalevents?month=" + month + "&day=" + day;
-        let JSON;
-        let conclusion = document.body;
+        let conclusion = document.getElementById("text");
         fetch(url, {
             method: 'GET',
             headers: {
                 'X-Api-Key': 'x+Gn2Rmh3SM6o6kmSvVasA==E7XlwsMp6TFty7qM',
-                'Accept': 'application/text'
+                'Accept': 'application/json'
             }
         })
-        .then(response => response.text())
-        .then(text => console.log(text));
+        .then(response => response.json())
+        .then(json => {
+            try {
+                fact(json);
+            }
+            catch(error) {
+                console.log("Ошибка: " + error);
+            }
+        });
+        function fact(object) {
+            let list_events = new Array(object.length);
+            let list_years = new Array(object.length);
+            for (let i = 0; i < object.length; i++) {
+                list_events[i] = object[i].event;
+                list_years[i] = object[i].year;
+            }
+            let random = parseInt(Math.random() * object.length);
+            if (list_years[random] < 0) {
+                conclusion.textContent += Math.abs(list_years[random]) + " before christ. " + list_events[random] + " \n";
+            }
+            else {
+                conclusion.textContent += list_years[random] + ". " + list_events[random] + " \n";
+            }
+        }
     }
 })();
--- js/test1.js
+++ js/test1.js
@@ -17,24 +17,28 @@
       if (document.getElementById("s10").value === "феофангрек") ans++;
       document.getElementById("b1").style.display = "none";
       document.getElementById("res").style.display = "none";
-      document.getElementById("balls").style.display = "block";
       document.getElementById("result").style.display = "block";
+      document.getElementById("comment").style.display = "block";
       document.getElementById("balls").textContent = "Ваш результат: " + ans + "/10";
       if (ans < 5) {
-        document.getElementById("result").textContent = "Оценка: 2";
-        document.getElementById("smail-2").style.display = "block";
+          document.getElementById("result").textContent = "Оценка: 2";
+          document.getElementById("comment").textContent = "Ничего страшного! У вас есть еще шанс изучить тему лучше в отличие от Наполеона, который упустил все, даже свои 100 дней во Франции. Надеемся, что у вас никогда больше не будет своего Ватерлоо.";
+          document.getElementById("smail-2").style.display = "block";
       }
       if (ans >= 5 && ans <= 6) {
-        document.getElementById("result").textContent = "Оценка: 3";
-        document.getElementById("smail-3").style.display = "block";
+          document.getElementById("result").textContent = "Оценка: 3";
+          document.getElementById("comment").textContent = "Наверное, для вас эта тема, как для ребят бочка с водой с картины Перова 'Тройка'. Но будем верить, что в будущем жизнь и обучение не будут в стиле критического реализма, а наступит эра авангардизма, где важна лишь суть вещей или где вовсе отказываются от смыслов.";
+          document.getElementById("smail-3").style.display = "block";
       }
       if (ans >= 7 && ans <= 8) {
-        document.getElementById("result").textContent = "Оценка: 4";
-        document.getElementById("smail-4").style.display = "block";
+          document.getElementById("result").textContent = "Оценка: 4";
+          document.getElementById("comment").textContent = "Вы - Ленин, у которого по окончании школы была лишь единственная четверка по логике. Думаю, что вы продолжите улучшать свой результат и когда-нибудь вы тоже совершите что-то невероятное (в хорошем смысле).";
+          document.getElementById("smail-4").style.display = "block";
       }
       if (ans > 8) {
-        document.getElementById("result").textContent = "Оценка: 5";
-        document.getElementById("smail-5").style.display = "block";
+          document.getElementById("result").textContent = "Оценка: 5";
+          document.getElementById("comment").textContent = "Пришёл! Увидел! Победил! Вы достойны триумфа, как Гай Юлий Цезарь! А может быть, вы настолько любите себя, что устроите себе 4 парадных въезда в Рим, как великий диктатор в 40-е гг. до н.э.";
+          document.getElementById("smail-5").style.display = "block";
       }
       let last_res = "Ваш прошлый результат: " + ans + "/10";
       localStorage.setItem("last-res", last_res);
